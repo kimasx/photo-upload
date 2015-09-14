3c60834a9f88623f6852dde138c33282b0349040
Sun Kim <sun@ralphandco.com>
2015-09-14 17:54:33 -0400

    add setUp in tests

--- app/tests.py
+++ app/tests.py
@@ -1,4 +1,15 @@
 import os
 import unittest
-import tempfile
-import routes
+from app import app
+
+
+class AppTests(unittest.TestCase):
+  def setUp(self):
+    self.client = app.test_client()
+
+  def test_img_response(self):
+    response = self.client.get('/upload')
+    print response.data
+
+if __name__ == '__main__':
+  unittest.main()
